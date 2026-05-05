e5f96e41a992f7e17b25454a7ebc7c571ccf30d3
Abul Hasan <habul6513@gmail.com>
2026-05-06 00:47:59 +0530

    refactor: update database connection initialization in db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2");
 
-// const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+db.connect((err) => {
+  if (err) {
+    console.error("❌ DB ERROR:", err);
+    process.exit(1); // important
+  }
+  console.log("✅ DB Connected");
 });
 
 module.exports = db;
